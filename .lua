9234a50e5032741b372ad2369308d7665a0ad661
malwareX17 <malwareX17@gmail.com>
2026-01-14 20:46:23 +0800

    Update .lua

--- .lua
+++ .lua
@@ -8,3 +8,15 @@ return warn'blocked the kick'
 end
 return old(self,...)
 end)
+
+local function Notify(title, text, duration)
+    pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = title or "Notification",
+            Text = text or "No Message",
+            Duration = duration or 5
+        })
+    end)
+end
+
+Notify("loaded", "yessir")
